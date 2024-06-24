7c58403efaec8a5f6959a2ee131b5474d53652b1
Ari J. Permana <arijatipermana@gmail.com>
2024-06-24 15:39:35 +0700

    add store product in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -29,4 +29,32 @@ class ProductController extends Controller
     public function create() : Vies {
         return view('products.create');
     }
+    
+    // store
+    public function store(Request $request): RedirectResponse {
+        // validate form
+        $request->validate([
+            'image'         =>  'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'         =>  'required|min:5',
+            'description'   =>  'required|min:10',
+            'price'         =>  'required|numeric',
+            'stock'         =>  'required|numeric'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/products', $image->hashName());
+
+        //create product
+        Product::create([
+            'image'         => $image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock
+        ]);
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data saved successfully!']);
+    }
 }
