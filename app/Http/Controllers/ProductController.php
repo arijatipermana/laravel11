146209e74390dd970e2722b29bb6d2d4ead30b19
Ari J. Permana <arijatipermana@gmail.com>
2024-06-24 14:28:15 +0700

    add comment to index ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,13 +8,20 @@ use App\Models\Product;
 // import return type view
 use Illuminate\View\View;
 
+// import return tyoe redurectResponse
+use Illuminate\Http\RedirectResponse;
+
+// import http request
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    // index
     public function index() : View {
+        // get all product
         $products = Product::latest()->paginate(10);
 
+        // render view with products
         return view('products.index', compact('products'));
     }
 }
